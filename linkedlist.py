1963c702c19a2ca4cad2018ea70bd0ced81e67c6
Zong <sophisycho@gmail.com>
2023-11-03 20:17:26 +0800

    Initial commit_v2

--- linkedlist.py
+++ linkedlist.py
@@ -21,7 +21,7 @@ class LinkedList:
         else:
             self.tail.next = new_node
             self.tail = new_node
-        self.length += 1
+        self.length += 1  # test
     # def prepend(self, value):
     # def insert(self, index, value):
 
